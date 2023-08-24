8eb1cd4a611c335c610942e81f43182fc4aad2c4
Wesley <wesleywillians@gmail.com>
2023-08-24 14:21:33 -0400

    aula 05

--- go/cmd/freight/main.go
+++ go/cmd/freight/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/devfullcycle/imersao14/go/internal/routes/entity"
@@ -11,11 +12,43 @@ import (
 	"github.com/devfullcycle/imersao14/go/internal/routes/usecase"
 	"github.com/devfullcycle/imersao14/go/pkg/kafka"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	routesCreated = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "routes_created_total",
+			Help: "Total number of created routes",
+		},
+	)
+
+	routesStarted = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "routes_started_total",
+			Help: "Total number of started routes",
+		},
+		[]string{"status"},
+	)
+
+	errorsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "errors_total",
+			Help: "Total number of errors",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(routesStarted)
+	prometheus.MustRegister(errorsTotal)
+	prometheus.MustRegister(routesCreated)
+}
 
 func main() {
 	msgChan := make(chan *ckafka.Message)
-	topics := []string{"routes"}
+	topics := []string{"route"}
 	servers := "host.docker.internal:9094"
 	go kafka.Consume(topics, servers, msgChan)
 
@@ -24,6 +57,10 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(":8080", nil)
+	}()
 	repository := repository.NewRouteRepositoryMysql(db)
 	freight := entity.NewFreight(10)
 	createRouteUseCase := usecase.NewCreateRouteUseCase(repository, freight)
@@ -38,14 +75,31 @@ func main() {
 			_, err := createRouteUseCase.Execute(input)
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesCreated.Inc()
 			}
 
-		case "RouteStarted", "RouteFinished":
+		case "RouteStarted":
 			input := usecase.ChangeRouteStatusInput{}
 			json.Unmarshal(msg.Value, &input)
 			_, err := changeRouteStatusUseCase.Execute(input)
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("started").Inc()
+			}
+
+		case "RouteFinished":
+			input := usecase.ChangeRouteStatusInput{}
+			json.Unmarshal(msg.Value, &input)
+			_, err := changeRouteStatusUseCase.Execute(input)
+			if err != nil {
+				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("finished").Inc()
 			}
 		}
 	}
--- nextjs/src/app/api/places/route.ts
+++ nextjs/src/app/api/places/route.ts
@@ -2,15 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
+  console.log(process.env.NEST_URL);
+
   const text = url.searchParams.get("text");
-  const response = await fetch(
-    `${process.env.NEST_URL}/places?text=${text}`,
-    {
-      next: {
-        revalidate: 60, //cache bem grande, valor bem grande
-      },
-    }
-  );
+  const response = await fetch(`${process.env.NEST_URL}/places?text=${text}`, {
+    next: {
+      revalidate: 60, //cache bem grande, valor bem grande
+    },
+  });
 
   return NextResponse.json(await response.json());
 }
